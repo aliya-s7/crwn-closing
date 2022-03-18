2151d1a8b604dbcffaa0c590eddf2e037e60ce88
aliya <sarymsakova.aliya@gmail.com>
2022-03-18 14:21:46 +0600

    updating our directory page with directory component and menu item

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import Directory from '../../components/directory/directory';
+
+
+import './homepage.scss';
+
+const HomePage = () =>  (
+        <Directory />
+           
+)
+
+export default HomePage;
